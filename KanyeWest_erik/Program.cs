e9c645b42efebd5464215ea0a2fcf576ae34f10f
Erik Hedman <erikdavid.hedman21@outlook.com>
2023-08-21 15:13:15 -0400

    Add project files.

---
+++ KanyeWest_erik/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
+var client = new HttpClient();
+
+var kanyeURL = "https://api.kanye.rest/";
+
+var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+var kanyeQuote = JObject.Parse(kanyeResponse)["quote"].ToString();
+
+
+Thread.Sleep(2000);
+    Console.WriteLine($"Kanye says, '{kanyeQuote}'");
+Console.Write(". ");
+Thread.Sleep(500);
+
+Console.Write(". ");
+Thread.Sleep(500);
+
+Console.Write();
+
